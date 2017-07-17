3dbe7d0514ad58e5c53ec49c193d5ee1d06c215e
29ff <nbhung1809@gmail.com>
2017-07-17 18:35:00 +0700

    nothing

--- client/controllers/shipmentController.js
+++ client/controllers/shipmentController.js
@@ -5,19 +5,21 @@ var db = require('../models/shipmentModel');
 
 
 module.exports = (app) => {
+    // GET QUOTE
     app.post('/client/getQuote', (req, res) => {
-        let data = req.body.data, lastId;
-        let stmt = db.prepare('INSERT INTO `users` (`ori_name`, `ori_email`, `ori_phone`, `ori_country_code`, `ori_locality`, `ori_postal_code`, ' +  
+        let data = req.body.data;
+        let stmt = db.prepare('INSERT INTO `quote` (`ori_name`, `ori_email`, `ori_phone`, `ori_country_code`, `ori_locality`, `ori_postal_code`, ' +  
         '`ori_address`, `dest_name`, `dest_email`, `dest_phone`, `dest_country_code`, `dest_locality`, `dest_postal_code`, `dest_address`, `dimensions_height`, ' + 
         '`dimensions_width`, `dimensions_length`, `dimensions_unit`, `gross_amount`, `gross_unit`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
 
         stmt.run(data.origin.contact.name, data.origin.contact.email, data.origin.contact.phone, data.origin.address.country_code, data.origin.address.locality, data.origin.address.postal_code,
         data.origin.address.address_line1, data.destination.contact.name, data.destination.contact.email, data.destination.contact.phone, data.destination.address.country_code, data.destination.address.locality, data.destination.address.postal_code,
         data.destination.address.address_line1, data.package.dimensions.height, data.package.dimensions.width, data.package.dimensions.length, data.package.dimensions.unit, data.package.grossWeight.amount, data.package.grossWeight.unit);
-
+        console.log('Quote inserted!');
+        // get last id on table quote
         let getLastId = () => {
             return new Promise((resolve, reject) => {
-                db.get('SELECT last_insert_rowid() as id', (err, result) => {
+                db.get('SELECT last_insert_rowid() as id FROM `quote`', (err, result) => {
                     if (err) {
                         return reject(err);
                     }
@@ -26,9 +28,10 @@ module.exports = (app) => {
             })
         };
 
+        // get response for user
         let resUser = (id) => {
             return new Promise((resolve, reject) => {
-                db.get('SELECT `id`, `gross_amount` FROM `users` WHERE id = ?', id, (err, result) => {
+                db.get('SELECT `id`, `gross_amount` FROM `quote` WHERE id = ?', id, (err, result) => {
                     if (err) {
                         return reject(err);
                     }
@@ -37,24 +40,50 @@ module.exports = (app) => {
             })
         };
         
+        // send response for user
         getLastId().then(result => {
             resUser(result.id).then(result => {
                 res.send({"data": result});
             }).catch(err => console.log(err));
         }).catch(err => console.log(err));
-        
+
     });
 
-
+    // CREAT SHIPMENT
     app.post('/client/createShipment', (req, res) => {
-        res.status(200).json({message: 'Create Shipment!'})
+        let data = req.body.data;
+        let stmt = db.prepare('INSERT INTO `shipment` (`ref`, `created_at`, `ori_name`, `ori_email`, `ori_phone`, `ori_country_code`, `ori_locality`, `ori_postal_code`, ' +  
+        '`ori_address`, `ori_organisation`, `dest_name`, `dest_email`, `dest_phone`, `dest_country_code`, `dest_locality`, `dest_postal_code`, `dest_address`, `dest_organisation`, `dimensions_height`, ' + 
+        '`dimensions_width`, `dimensions_length`, `dimensions_unit`, `gross_amount`, `gross_unit`, `quote_id`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+
+        // generate random number
+        let randomNum = Math.random().toString().substring(2, 12);
+
+        // get time
+        let createTime = new Date();
+
+        console.log(createTime);
+
+        stmt.run(randomNum, createTime, data.origin.contact.name, data.origin.contact.email, data.origin.contact.phone, data.origin.address.country_code, data.origin.address.locality, data.origin.address.postal_code,
+        data.origin.address.address_line1, data.origin.address.organisation, data.destination.contact.name, data.destination.contact.email, data.destination.contact.phone, data.destination.address.country_code, data.destination.address.locality, data.destination.address.postal_code,
+        data.destination.address.address_line1, data.destination.address.organisation, data.package.dimensions.height, data.package.dimensions.width, data.package.dimensions.length, data.package.dimensions.unit, data.package.grossWeight.amount, data.package.grossWeight.unit, data.quote.id);
+        console.log('Shipment inserted!');
+        
+        // get response for user
+        let resUser = () => {
+
+        }
+
+        res.send('Done!');
     });
 
-
+    // GET SHIPMENT
     app.get('/client/getShipment', (req, res) => {  
         res.status(200).json({message: 'Get Shipment!'})
     });
 
+
+    // DELETE SHIPMENT
     app.delete('/client/deleteShipment', (req, res) => {
         res.status(200).json({message: 'Delele Shipment!'})
     });
--- client/models/shipmentModel.js
+++ client/models/shipmentModel.js
@@ -16,13 +16,15 @@ var dataRate = require('../data/rate.json');
 db.serialize(() => {
 
     // Optional installation for newly created database
+    // create rate table
     db.run('CREATE TABLE IF NOT EXISTS `rate` (' +
     '`weight` TEXT NOT NULL,' +
     '`price` DOUBLE NOT NULL,' +
     '`fromCountry` TEXT NOT NULL,' + 
     '`toCountry` TEXT NOT NULL)');
 
-    db.run('CREATE TABLE IF NOT EXISTS `users` (' +
+    // create quote table
+    db.run('CREATE TABLE IF NOT EXISTS `quote` (' +
     '`id` INTEGER PRIMARY KEY NOT NULL, ' +
     '`ori_name` TEXT NOT NULL, ' +
     '`ori_email` TEXT NOT NULL, ' + 
@@ -44,7 +46,36 @@ db.serialize(() => {
     '`dimensions_unit` INTEGER NOT NULL, ' +
     '`gross_amount` INTEGER NOT NULL, ' +
     '`gross_unit` TEXT NOT NULL)');
- 
+    
+    // create shipment table
+    db.run('CREATE TABLE IF NOT EXISTS `shipment` (' +
+    '`ref` TEXT NOT NULL, ' +
+    '`created_at` DATETIME NOT NULL, ' +
+    '`ori_name` TEXT NOT NULL, ' +
+    '`ori_email` TEXT NOT NULL, ' + 
+    '`ori_phone` TEXT NOT NULL, ' +
+    '`ori_country_code` TEXT NOT NULL, ' +
+    '`ori_locality` TEXT NOT NULL, ' +
+    '`ori_postal_code` TEXT NOT NULL, ' +
+    '`ori_address` TEXT NOT NULL, ' +
+    '`ori_organisation` BOOLEAN NOT NULL, ' +
+    '`dest_name` TEXT NOT NULL, ' +
+    '`dest_email` TEXT NOT NULL, ' + 
+    '`dest_phone` TEXT NOT NULL, ' +
+    '`dest_country_code` TEXT NOT NULL, ' +
+    '`dest_locality` TEXT NOT NULL, ' +
+    '`dest_postal_code` TEXT NOT NULL, ' +
+    '`dest_address` TEXT NOT NULL, ' +
+    '`dest_organisation` BOOLEAN NOT NULL, ' +
+    '`dimensions_height` INTEGER NOT NULL, ' + 
+    '`dimensions_width` INTEGER NOT NULL, ' +
+    '`dimensions_length` INTEGER NOT NULL, ' +
+    '`dimensions_unit` INTEGER NOT NULL, ' +
+    '`gross_amount` INTEGER NOT NULL, ' +
+    '`gross_unit` TEXT NOT NULL, ' +
+    '`quote_id` INTEGER NOT NULL, ' +
+    'FOREIGN KEY (`quote_id`) REFERENCES `quote`(`id`))');
+
     // Insert data
     var stmt = db.prepare('INSERT INTO `rate` (`weight`, `price`, `fromCountry`, `toCountry`) ' + 
         'VALUES (?, ?, ?, ?) ');
